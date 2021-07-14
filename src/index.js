fda98b5407b9000f5fdc5b2481e9ea4a2465f650
Leandro <officialdevmi@gmail.com>
2021-07-14 10:12:25 -0700

    Add function to populate HTML list

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
 import './style.css'
+
+let taskArr = [{
+  description: "Do 30 min cardio",
+  completed: false,
+  index: 1
+}, {
+  description: "Meditate 10 min",
+  completed: true,
+  index: 2
+}, {
+  description: "Do dinner",
+  completed: false,
+  index: 3
+}];
+  
+function displayTask() {
+  for (let i = 0; i < taskArr.length; i++) {
+    const cont = document.getElementById('main-container');
+    const itemContainer = document.createElement('div');
+    itemContainer.setAttribute('class', 'item-container')
+    const item = document.createElement('p');
+    item.setAttribute('class', 'item');
+    item.innerHTML = taskArr[i].description;
+    itemContainer.appendChild(item);
+    cont.appendChild(itemContainer);
+  };
+}
+  
+document.addEventListener('DOMContentLoaded', displayTask);
