0476bfa7d3323342a19c51f5677d221d932dafce
Leandro <officialdevmi@gmail.com>
2021-07-14 13:13:09 -0700

    Add styles to ToDo List

--- src/index.js
+++ src/index.js
@@ -18,13 +18,35 @@ function displayTask() {
   for (let i = 0; i < taskArr.length; i++) {
     const cont = document.getElementById('main-container');
     const itemContainer = document.createElement('div');
-    itemContainer.setAttribute('class', 'item-container')
+    itemContainer.setAttribute('class', 'item-container');
+    const squareContainer = document.createElement('div');
+    squareContainer.setAttribute('class', 'square-container');
+    const icon = document.createElement('i');
+    icon.setAttribute('class', 'far fa-square');
+    squareContainer.appendChild(icon);
     const item = document.createElement('p');
     item.setAttribute('class', 'item');
     item.innerHTML = taskArr[i].description;
-    itemContainer.appendChild(item);
+    squareContainer.appendChild(item);
+    itemContainer.appendChild(squareContainer);
+    const dotsContainer = document.createElement('div');
+    dotsContainer.setAttribute('class', 'dots-container')
+    const iconTwo = document.createElement('i');
+    iconTwo.setAttribute('class', 'fas fa-ellipsis-v');
+    dotsContainer.appendChild(iconTwo);
+    itemContainer.appendChild(dotsContainer);
     cont.appendChild(itemContainer);
   };
+  if (taskArr.length > 0) {
+    const cont = document.getElementById('main-container');
+    const itemContainer = document.createElement('div');
+    itemContainer.setAttribute('class', 'item-container');
+    itemContainer.setAttribute('id', 'clear');
+    const clear = document.createElement('p');
+    clear.innerHTML = 'Clear all completed';
+    itemContainer.appendChild(clear);
+    cont.appendChild(itemContainer);
+  }
 }
   
 document.addEventListener('DOMContentLoaded', displayTask);
